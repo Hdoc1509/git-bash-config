94a5e53da4747e24846653e8b87ec4858e47b9cc
hdoc <blueguitarist1520@gmail.com>
2022-11-06 19:24:10 -0500

    Feat: Add  variable to source_files_in function

--- utils.sh
+++ utils.sh
@@ -1,9 +1,10 @@
 source_files_in () {
-  local folder=~/.config/git-bash/$1;
+  local folder=$1;
+  local prefix=$2;
 
-  for file in "$folder"/*.sh; do
+  for file in $folder/*.sh; do
     if [[ -f $file ]]; then
-      echo -ne "${lightpurple}Sourcing${nocolor} ${lightcyan}${1}/${file##*/}${nocolor} ... "
+      echo -ne "${lightpurple}Sourcing${nocolor} ${lightcyan}${prefix}/${file##*/}${nocolor} ... "
       source $file && echo -e "${lightgreen}done" || echo -e "${red}FAILED"
     else
       echo "${orange}Warning: Some problem with \"$file\""
