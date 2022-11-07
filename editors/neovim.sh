63a0fbf23b270952622678201fe62b6c7f77a706
hdoc <blueguitarist1520@gmail.com>
2022-11-06 19:19:46 -0500

    Feat: 'nv' function run specific command according to msys variable

--- editors/neovim.sh
+++ editors/neovim.sh
@@ -4,7 +4,17 @@ nv_flag () {
   echo "$([[ "$1" == '.' ]] && echo "${nvFzfFlag} ." || echo "-p $@")"
 }
 
-nv () { eval "winpty nvim $(nv_flag "$@")"; }
+nv () {
+  local to_execute=''
+
+  if [[ "$OSTYPE" == 'linux-gnu'* ]]; then
+    to_execute='nvim'
+  elif [[ "$OSTYPE" == 'msys' ]]; then
+    to_execute='winpty nvim'
+  fi
+
+  eval "${to_execute} $(nv_flag "$@")"
+}
 nvq () { eval "nvim-qt -- $(nv_flag "$@")"; }
 nvqNb () { eval "nvim-qt -- $(nv_flag "@") & /dev/null"; }
 nvd () { eval "neovide --multigrid -- $(nv_flag "$@")"; }
