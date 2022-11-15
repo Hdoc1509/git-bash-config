5cb954a8b5a948bee61bcaeca67b4bc832452215
hdoc <blueguitarist1520@gmail.com>
2022-11-14 19:48:20 -0500

    Refactor: Update functions in utils.sh
    * Rename set_builtin_theme to set_posh_theme
    * Simplify logic in set_theme

--- utils.sh
+++ utils.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-set_theme () {
-  local path=$1
-  local file=$2
-  eval "$(oh-my-posh init bash --config "$path"/"$file")"
-}
+set_theme () { eval "$(oh-my-posh init bash --config "$1"/"$2".omp.json)"; }
 
-set_builtin_theme () { set_theme "$POSH_THEMES_PATH" "$1"; }
+set_posh_theme () { set_theme "$POSH_THEMES_PATH" "$1"; }
 
 set_custom_theme () { set_theme ~/.config/git-bash/custom-themes "$1"; }
