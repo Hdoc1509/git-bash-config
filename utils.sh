6048d17674bfd925d9dd75c5cd3bfba8e5a58592
hdoc <blueguitarist1520@gmail.com>
2022-11-18 22:18:00 -0500

    Style: Format utils.sh

--- utils.sh
+++ utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-set_theme () { eval "$(oh-my-posh init bash --config "$1"/"$2".omp.json)"; }
+set_theme() { eval "$(oh-my-posh init bash --config "$1"/"$2".omp.json)"; }
 
-set_posh_theme () { set_theme "$POSH_THEMES_PATH" "$1"; }
+set_posh_theme() { set_theme "$POSH_THEMES_PATH" "$1"; }
 
-set_custom_theme () { set_theme ~/.config/git-bash/custom-themes "$1"; }
+set_custom_theme() { set_theme ~/.config/git-bash/custom-themes "$1"; }
